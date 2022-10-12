35e79ee7f2334f7d84b6b24ef9e784a07a20b7c8
kevmill29 <96067818+kevmill29@users.noreply.github.com>
2022-10-11 20:30:42 -0400

    Add files via upload
    Changed HTML Line GUEST header to AWAY

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
+/* HOME */
 let homeScoreEl = document.getElementById('home-score')
 let homeScoreBtn1 = document.getElementById("home-add1-btn")
 let homeScoreBtn2 = document.getElementById("home-add2-btn")
 let homeScoreBtn3 = document.getElementById("home-add3-btn")
-
-let guestScoreEl = document.getElementById('guest-score')
-let guestScoreBtn1 = document.getElementById("guest-add1-btn")
-let guestScoreBtn2 = document.getElementById("guest-add2-btn")
-let guestScoreBtn3 = document.getElementById("guest-add3-btn")
-
-
 let homeScoreBtn4 = document.getElementById("home-sub1-btn")
 let homeScoreBtn5 = document.getElementById("home-sub2-btn")
 let homeScoreBtn6 = document.getElementById("home-sub3-btn")
 
 
+/* GUEST */
+let guestScoreEl = document.getElementById('guest-score')
+let guestScoreBtn1 = document.getElementById("guest-add1-btn")
+let guestScoreBtn2 = document.getElementById("guest-add2-btn")
+let guestScoreBtn3 = document.getElementById("guest-add3-btn")
 let guestScoreBtn4 = document.getElementById("guest-sub1-btn")
 let guestScoreBtn5 = document.getElementById("guest-sub2-btn")
 let guestScoreBtn6 = document.getElementById("guest-sub3-btn")
 
+/* RESET */
 let homeResetBtn = document.getElementById("hreset-score")
 let guestResetBtn = document.getElementById("greset-score")
 
 let homeScore = 0
 let guestScore = 0
+
+/* HOME FUNCTION SECTION */
 
 function homeAdd1(){
  homeScore += 1
@@ -39,6 +41,27 @@ function homeAdd3(){
  homeScoreEl.textContent = homeScore 
 }
 
+function homeSub1(){
+ homeScore -= 1
+ homeScoreEl.textContent = homeScore 
+}
+
+function homeSub2(){
+ homeScore -= 2
+ homeScoreEl.textContent = homeScore 
+}
+
+function homeSub3(){
+ homeScore -= 3
+ homeScoreEl.textContent = homeScore} 
+ 
+ function homeReset(){
+    homeScore = 0
+    homeScoreEl.textContent = homeScore
+}
+ 
+/* AWAY FUNCTION SECTION */
+
 function guestAdd1(){
  guestScore += 1
  guestScoreEl.textContent = guestScore 
@@ -54,21 +77,6 @@ function guestAdd3(){
  guestScoreEl.textContent = guestScore 
 }
 
-function homeSub1(){
- homeScore -= 1
- homeScoreEl.textContent = homeScore 
-}
-
-function homeSub2(){
- homeScore -= 2
- homeScoreEl.textContent = homeScore 
-}
-
-function homeSub3(){
- homeScore -= 3
- homeScoreEl.textContent = homeScore 
-}
-
 function guestSub1(){
  guestScore -= 1
  guestScoreEl.textContent = guestScore 
@@ -82,11 +90,6 @@ function guestSub2(){
 function guestSub3(){
  guestScore -= 3
  guestScoreEl.textContent = guestScore 
-}
-    
-function homeReset(){
-    homeScore = 0
-    homeScoreEl.textContent = homeScore
 }
 
 function guestReset(){
